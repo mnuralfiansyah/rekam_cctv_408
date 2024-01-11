d2fcf3c61c659aa1d4611ca39d7c56732550f7eb
mnuralfiansyah <mnuralfiansyah@gmail.com>
2024-01-11 14:21:43 +0800

    :hammer: x

--- video_to_img.py
+++ video_to_img.py
@@ -8,7 +8,7 @@ count = 0
 while success:
     success,image = vidcap.read()
 
-    if count % 25 == 0:
+    if count % 250 == 0:
         cv2.imwrite("gambar\\408_frame_"+str(count)+"_"+str(datetime.now().strftime('%Y_%m_%d_%H_%M_%S'))+".png", image)     # save frame as JPEG file      
         print('Read a new frame: ', success)
         print("gambar\\408_frame_"+str(count)+"_"+str(datetime.now().strftime('%Y_%m_%d_%H_%M_%S'))+".png")
