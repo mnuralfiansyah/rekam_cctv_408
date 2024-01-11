329a588e7078b7d42d110afd8a1f5348b7f0ca62
mnuralfiansyah <mnuralfiansyah@gmail.com>
2024-01-11 14:19:02 +0800

    :hammer: x

---
+++ video_to_img.py
@@ -0,0 +1,15 @@
+import cv2
+from datetime import datetime
+
+
+vidcap = cv2.VideoCapture('rtsp://admin:@192.168.1.13:2300/live')
+success,image = vidcap.read()
+count = 0
+while success:
+    success,image = vidcap.read()
+
+    if count % 25 == 0:
+        cv2.imwrite("gambar\\408_frame_"+str(count)+"_"+str(datetime.now().strftime('%Y_%m_%d_%H_%M_%S'))+".png", image)     # save frame as JPEG file      
+        print('Read a new frame: ', success)
+        print("gambar\\408_frame_"+str(count)+"_"+str(datetime.now().strftime('%Y_%m_%d_%H_%M_%S'))+".png")
+    count += 1
